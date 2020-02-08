602396025464352d114fc679885954620bb466b4
Chen Peng <pizer.chen@gmail.com>
2020-02-08 11:54:13 +0800

    remove imports

--- src/main/java/com/github/iceant/xrocker/spring/boot/starter/XRockerAutoConfiguration.java
+++ src/main/java/com/github/iceant/xrocker/spring/boot/starter/XRockerAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.github.iceant.xrocker.spring.boot.starter;
 
-import com.fizzed.rocker.reload.ReloadingRockerBootstrap;
 import com.fizzed.rocker.runtime.RockerBootstrap;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
